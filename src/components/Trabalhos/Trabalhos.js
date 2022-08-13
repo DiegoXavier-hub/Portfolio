a6067a44802e399660e9078a9d512f258527eb4b
DiegoXavier-hub <diegohenriquexavier2003@gmail.com>
2022-08-12 21:38:53 -0300

    upload files

--- src/components/Trabalhos/Trabalhos.js
+++ src/components/Trabalhos/Trabalhos.js
@@ -1,73 +1,41 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './Trabalhos.css'
 
 function Trabalhos(){
 
-    let trabalhos
+    const [trabalhos, setTrabalhos] = useState()
 
     useEffect(() =>{
-        trabalhos = document.getElementById('Trabalhos')
+        setTrabalhos(document.getElementById('Trabalhos'))
     }, [])
 
     function backgroundAnim(param) {
-        switch (parseInt(param)) {
+        switch (parseInt(param)){
             case 1:
-                trabalhos.classList.remove('b2')
-                trabalhos.classList.remove('b3')
-                trabalhos.classList.remove('b4')
-                trabalhos.classList.remove('b5')
-                trabalhos.classList.remove('b6')
-                trabalhos.classList.add('b1')
+                trabalhos.className = "b1"
                 break;
-            case 2: 
-                trabalhos.classList.remove('b1')
-                trabalhos.classList.remove('b3')
-                trabalhos.classList.remove('b4')
-                trabalhos.classList.remove('b5')
-                trabalhos.classList.remove('b6')
-                trabalhos.classList.add('b2')
+                case 2: 
+                trabalhos.className = "b2"
                 break;
             case 3:
-                trabalhos.classList.remove('b1')
-                trabalhos.classList.remove('b2')
-                trabalhos.classList.remove('b4')
-                trabalhos.classList.remove('b5')
-                trabalhos.classList.remove('b6')
-                trabalhos.classList.add('b3')
+                trabalhos.className = "b3"
                 break;
             case 4:
-                trabalhos.classList.remove('b1')
-                trabalhos.classList.remove('b2')
-                trabalhos.classList.remove('b3')
-                trabalhos.classList.remove('b5')
-                trabalhos.classList.remove('b6')
-                trabalhos.classList.add('b4')
+                trabalhos.className = "b4"
                 break;
             case 5: 
-                trabalhos.classList.remove('b1')
-                trabalhos.classList.remove('b2')
-                trabalhos.classList.remove('b3')
-                trabalhos.classList.remove('b4')
-                trabalhos.classList.remove('b6')
-                trabalhos.classList.add('b5')
+                trabalhos.className = "b5"
                 break;
             case 6:
-                trabalhos.classList.remove('b1')
-                trabalhos.classList.remove('b2')
-                trabalhos.classList.remove('b3')
-                trabalhos.classList.remove('b4')
-                trabalhos.classList.remove('b5')
-                trabalhos.classList.add('b6')
+                trabalhos.className = "b6"
                 break;
             default:
                 break;
         }
     }
 
-
-
     return(
-        <section id="Trabalhos" className="Trabalhos">
+        <section id="Trabalhos">
             <div>
                 <section className="work" onMouseOver={()=> backgroundAnim(1)}>
                     
